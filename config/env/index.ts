007fb3db6f6418a50a0efa66219ff07eb688ff55
Samir El Hassan <samirelhassann@gmail.com>
2024-01-29 21:12:49 -0300

    Adjusting unit tests

--- config/env/index.ts
+++ config/env/index.ts
@@ -20,6 +20,8 @@ const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
   console.error("Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables");
 }
 
 export const env = _env.data;
